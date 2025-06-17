c1e22a6e093a6336f245d51de1404e7bf4a7f07c
Felipe Rafael Barros <gitfelipebarros@gmail.com>
2025-06-17 11:05:24 -0300

    Implementa listagem clicável de Pokémons
    - Integração com PokeApiService
    - Roteamento para details/:name
    - Layout básico com ion-list

--- pokeapp/src/app/home/home.page.ts
+++ pokeapp/src/app/home/home.page.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem } from '@ionic/angular/standalone';
+import { PokeApiService } from '../services/poke-api.service';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent,
+    IonList,
+    IonItem, 
+    CommonModule,
+    RouterLink 
+  ],
 })
 export class HomePage {
-  constructor() {}
+  pokemons: any[] = [];
+
+  constructor(private pokeApi: PokeApiService) {}
+
+  async loadPokemons() {
+    this.pokemons = await this.pokeApi.getPokemonList();
+  }
+
+  ionViewDidEnter() {
+    this.loadPokemons();
+  }
 }
