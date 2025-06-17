ba023e44640bb3acacac1c638f75a0eb6d944497
Felipe Rafael Barros <gitfelipebarros@gmail.com>
2025-06-17 09:40:24 -0300

    Implementa PokeApiServece com axios
    - Métodos getPokemonList e getPokemonDetails
    - Configuração base da URL da API

--- pokeapp/src/app/services/poke-api.service.ts
+++ pokeapp/src/app/services/poke-api.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeApiService {
 
-  constructor() { }
+  private apiUrl = 'https://pokeapi.co/api/v2'; // Conecta ao PokeAPI
+
+  async getPokemonList(offset = 0, limit = 20){
+    const response = await axios.get(`${this.apiUrl}/pokemon?offset=${offset}&limit=${limit}`);
+    return response.data.results;
+  }
+
+  async getPokemonDetails(name : string){
+    const response = await axios.get(`${this.apiUrl}/pokemon/${name}`);
+    return response.data;
+  }
+
+  
 }
